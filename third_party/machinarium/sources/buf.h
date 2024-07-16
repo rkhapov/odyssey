fd19f260e23b97cd51e47d734385989e18cb3fb6
Roman Khapov <rkhapov@yandex-team.ru>
2024-07-16 11:29:57 +0500

    third_patry/buf.h: fix usage of buf->start after realloc
    This is not a bug, but this can confuse some static analyzers.
    
    Signed-off-by: Roman Khapov <rkhapov@yandex-team.ru>

--- third_party/machinarium/sources/buf.h
+++ third_party/machinarium/sources/buf.h
@@ -57,14 +57,15 @@ static inline int mm_buf_ensure(mm_buf_t *buf, int size)
 	if (buf->end - buf->pos >= size)
 		return 0;
 	int sz = mm_buf_size(buf) * 2;
-	int actual = mm_buf_used(buf) + size;
+	int used = mm_buf_used(buf);
+	int actual = used + size;
 	if (actual > sz)
 		sz = actual;
 	char *p;
 	p = realloc(buf->start, sz);
 	if (p == NULL)
 		return -1;
-	buf->pos = p + (buf->pos - buf->start);
+	buf->pos = p + used;
 	buf->end = p + sz;
 	buf->start = p;
 	assert((buf->end - buf->pos) >= size);
